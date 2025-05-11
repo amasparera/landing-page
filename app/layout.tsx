fdc0ed4e36aa24fa6b0537da72355b9053733b88
amasparera <amasparera@gmail.com>
2025-05-11 08:45:14 +0700

    fix header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Amas Parera',
+  description: 'Programer Jos Mantap',
   generator: 'v0.dev',
 }
 
